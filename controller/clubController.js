9dcba27b4c9f0676e5fda358b03e7564d7fe3a64
sonchaegeon <thscorjs1002@naver.com>
2021-01-02 21:21:44 +0900

    UPDATE; query update

--- controller/clubController.js
+++ controller/clubController.js
@@ -1,7 +1,11 @@
 const db = require('../models');
 
 exports.showList = async (req, res) => {
-    db.sequelize.query("SELECT club.name, title FROM club_tag INNER JOIN club ON club_tag.club_id = club.id INNER JOIN tag ON club_tag.tag_id = tag.id;").then((results, metadata) => {
+    db.sequelize.query("SELECT club.name, tag FROM club_tag INNER JOIN club ON club_tag.club_id = club.id INNER JOIN tag ON club_tag.tag_id = tag.id;").then((results, metadata) => {
         res.json(results[0]);
     });
+}
+
+exports.supplyList = async (req, res) => {
+    console.log(req.query);
 }
--- routes/clubRouter.js
+++ routes/clubRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const clubController = require('../controller/clubController.js');
 
 router.get('/list', clubController.showList);
+router.get('/:club_id/supply/list', clubController.supplyList);
 
 module.exports = router;
