4dd72d2c83d6a042ffd1eb9ffbffc9e7f37061f9
sonchaegeon <thscorjs1002@naver.com>
2021-01-24 01:37:04 +0900

    CREATE; app.ts

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+
+import express , { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+import { HttpError, apiNotFoundError } from "./error";
+import config from "./config";
+
+const app: express.Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send("<h1>Hello;</h1>");
+})
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(apiNotFoundError);
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    })
+})
+
+app.listen(config.port, () => {
+    console.log(`server running on port ${config.port}`);
+})
