a15466ca2ee006c796b17697db9bb30afa72ed77
sonchaegeon <thscorjs1002@naver.com>
2021-01-24 01:57:33 +0900

    CREATE; ormconfig.ts

---
+++ ormconfig.ts
@@ -0,0 +1,12 @@
+import config from './src/config';
+
+export = {
+    type: "mysql",
+    host: config.mysql.dbHost,
+    port: Number(config.mysql.dbPort),
+    username: config.mysql.dbUser,
+    password: config.mysql.dbPass,
+    database: config.mysql.dbName,
+    synchronize: true,
+    entities: []
+} 
