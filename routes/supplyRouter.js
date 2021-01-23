d2cee08917616e51d127d70bf8276d04728ed95c
sonchaegeon <thscorjs1002@naver.com>
2021-01-24 00:53:54 +0900

    RESET

--- routes/supplyRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supplyController = require('../controller/supplyController.js');
-
-router.get('/:supply_id/invoice', supplyController.getInvoice);
-router.get('/list', supplyController.supplyList);
-
-
-module.exports = router;
