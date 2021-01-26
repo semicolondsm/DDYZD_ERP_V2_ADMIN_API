1ccfbc972f349d4511f39b2ab1a01ccbfcdabc4d
sonchaegeon <thscorjs1002@naver.com>
2021-01-27 02:30:27 +0900

    CREATE; admin auth router

---
+++ src/api/routers/auth.ts
@@ -0,0 +1,19 @@
+import { Request, Response, Router } from "express";
+import AuthController from "../../controllers/auth";
+import validate, { Property } from "../middlewares/paramValidation";
+import { loginSchema } from "../middlewares/paramValidation/schemas";
+import tryCatchHandler from "../middlewares/tryCatchHandler";
+
+const route = Router();
+
+export default (app: Router) => {
+    const authController = new AuthController();
+
+    app.use("/admin", route);
+
+    route.post(
+        "/auth",
+        validate({ schema: loginSchema, property: Property.BODY }),
+        tryCatchHandler(authController.login)
+    );
+}
