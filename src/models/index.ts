9244036f1a470def8cc9bd0c24cdbabf599e2995
sonchaegeon <thscorjs1002@naver.com>
2021-01-27 02:28:20 +0900

    UPDATE; export admin model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import { Club } from "./Club";
 import { Tag } from "./Tag";
 import { ClubHasTag } from "./ClubHasTag";
+import { Admin } from "./Admin";
 
 export {
     Club,
     Tag,
-    ClubHasTag
+    ClubHasTag,
+    Admin
 }
