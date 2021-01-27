da3a381ac205213724a54c8d8262ebe841c5c82c
sonchaegeon <thscorjs1002@naver.com>
2021-01-27 17:14:37 +0900

    UPDATE; add token errors

--- src/error.ts
+++ src/error.ts
@@ -8,13 +8,19 @@ class HttpError extends Error {
     }
 }
 
-const apiNotFoundError = new HttpError("API not found", 404);
 const invalidParameterError = new HttpError("Invalid parameter", 400);
+const invalidTokenError = new HttpError("Invalid token", 401);
 const invalidLoginInformationError = new HttpError("Invalid id or password", 403);
+const notAccessTokenError = new HttpError("Authorization is not access token", 403);
+const apiNotFoundError = new HttpError("API not found", 404);
+const expiredTokenError = new HttpError("Expired token", 410);
 
 export {
     HttpError,
     apiNotFoundError,
     invalidParameterError,
-    invalidLoginInformationError
+    invalidLoginInformationError,
+    invalidTokenError,
+    notAccessTokenError,
+    expiredTokenError,
 }
