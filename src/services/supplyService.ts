126683f4d8e5ab85eb77f7f1d55fb4e34ab63f35
sonchaegeon <thscorjs1002@naver.com>
2021-01-27 22:46:39 +0900

    CREATE; supply service

---
+++ src/services/supplyService.ts
@@ -0,0 +1,15 @@
+import SupplyRepository from "../repositories/supplyRepository";
+import { supplyAlreadyCheckError, supplyNotFoundError } from "../error";
+
+export default class SupplyService {
+
+    public async supplyAccept (supply_id: number) {
+        if(!await SupplyRepository.getQueryRepository().findSupplyById(supply_id)) {
+            throw supplyNotFoundError;
+        }
+        if(!await SupplyRepository.getQueryRepository().findSupplyByStatus(supply_id, 2)) {
+            throw supplyAlreadyCheckError;
+        }
+        await SupplyRepository.getQueryRepository().supplyAccept(supply_id);
+    }
+}
