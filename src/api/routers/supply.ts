d8dfe08bf8d1b6862e4c2dd9b34995c599e8fa97
sonchaegeon <thscorjs1002@naver.com>
2021-01-27 22:48:53 +0900

    CREATE; supply router

---
+++ src/api/routers/supply.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import SupplyController from "../../controllers/supply";
+import tokenVerification from "../middlewares/tokenVerification";
+import tryCatchHandler from "../middlewares/tryCatchHandler";
+import validate, { Property } from "../middlewares/paramValidation";
+import { supplySchema } from "../middlewares/paramValidation/schemas";
+const route = Router();
+
+export default (app: Router) => {
+    const supplyController = new SupplyController();
+
+    app.use("/supply", route);
+
+    route.patch(
+        "/:supply_id/accept",
+        tokenVerification, 
+        validate({ schema: supplySchema, property: Property.PARAMS }),
+        tryCatchHandler(supplyController.supplyAccept)
+    );
+}
