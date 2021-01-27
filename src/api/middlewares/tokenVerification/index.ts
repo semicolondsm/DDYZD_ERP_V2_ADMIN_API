f9431a0fb57532adf065e2c1db03aaba5e38ac5b
sonchaegeon <thscorjs1002@naver.com>
2021-01-27 17:16:40 +0900

    CREATE; token verification middleware

---
+++ src/api/middlewares/tokenVerification/index.ts
@@ -0,0 +1,23 @@
+import config from "../../../config";
+import { Request, Response, NextFunction } from "express";
+import verify from "./verify";
+import { TokenExpiredError } from "jsonwebtoken";
+import { expiredTokenError, invalidTokenError, notAccessTokenError } from "../../../error";
+
+export default (req: Request, res: Response, next: NextFunction) => {
+    const token = req.get("Authorization");
+    try {
+        res.locals.payload = verify({
+            token,
+            jwtSecret: config.jwtSecret
+        });
+        next();
+    } catch (err) {
+        if (err instanceof TokenExpiredError) {
+            return next(expiredTokenError);
+        } else if (err === notAccessTokenError) {
+            return next(err);
+        }
+        next(invalidTokenError);
+    }
+}
