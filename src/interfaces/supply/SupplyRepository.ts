7a3ac2dae8cf23ee83b11c9437e6bcf4743c7f80
sonchaegeon <thscorjs1002@naver.com>
2021-01-29 04:07:35 +0900

    UPDATE; supplyDeny, findInvoiceById interface

--- src/interfaces/supply/SupplyRepository.ts
+++ src/interfaces/supply/SupplyRepository.ts
@@ -2,6 +2,8 @@ import { Supply } from "../../models";
 
 export default interface SupplyRepository {
     supplyAccept(id: number): Promise<void>;
+    supplyDeny(id: number): Promise<void>;
     findSupplyById(id: number): Promise<Supply>;
     findSupplyByStatus(id:number, status: number): Promise<Supply>;
+    findInvoiceById(id: number): Promise<Supply>;
 }
