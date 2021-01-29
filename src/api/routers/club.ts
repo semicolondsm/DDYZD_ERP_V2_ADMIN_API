7682b5a46abe3ef1d49ff6bfcdd7d1fe47962f2b
sonchaegeon <thscorjs1002@naver.com>
2021-01-29 18:07:20 +0900

    UPDATE; club supply list router
    validate params, query

--- src/api/routers/club.ts
+++ src/api/routers/club.ts
@@ -3,7 +3,7 @@ import ClubController from "../../controllers/club";
 import tokenVerification from "../middlewares/tokenVerification";
 import tryCatchHandler from "../middlewares/tryCatchHandler";
 import validate, { Property } from "../middlewares/paramValidation";
-import { budgetSchema } from "../middlewares/paramValidation/schemas";
+import { budgetSchema, stateSchema } from "../middlewares/paramValidation/schemas";
 const route = Router();
 
 export default (app: Router) => {
@@ -16,9 +16,17 @@ export default (app: Router) => {
         tryCatchHandler(clubController.clubList)
     );
 
+    route.get(
+        "/:club_id/supply/list",
+        validate({ schema: clubSchema, property: Property.PARAMS }),
+        validate({ schema: stateSchema, property: Property.QUERY }),
+        tryCatchHandler(clubController.clubSupplyList)
+    )
+
     route.patch(
         "/:club_id/budget",
         tokenVerification,
+        validate({ schema: clubSchema, property: Property.PARAMS }),
         validate({ schema: budgetSchema, property: Property.BODY }),
         tryCatchHandler(clubController.setBudget)
     );
