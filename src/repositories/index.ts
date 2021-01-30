5d7645d4b8fdf8e6075b39956c1c735bec522564
sonchaegeon <thscorjs1002@naver.com>
2021-01-31 00:35:40 +0900

    CREATE: club head repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,7 +1,13 @@
 import AdminRepositoryImpl from "./adminRepository";
 import SupplyRepositoryImpl from "./supplyRepository";
+import ClubRepositoryImpl from "./clubRepository";
+import UserRepositoryImpl from "./userRepository";
+import ClubHeadRepositoryImpl from "./clubHeadRepository";
 
 export {
     AdminRepositoryImpl,
-    SupplyRepositoryImpl
+    SupplyRepositoryImpl,
+    ClubRepositoryImpl,
+    UserRepositoryImpl,
+    ClubHeadRepositoryImpl,
 }
