d06bcd91e4a2bdb89e907929010e15f0a044bec9
sonchaegeon <thscorjs1002@naver.com>
2021-01-31 00:35:03 +0900

    UPDATE; new club service

--- src/services/clubService.ts
+++ src/services/clubService.ts
@@ -1,28 +1,42 @@
-import ClubRepository from "../repositories/clubRepository";
-import { clubNotFoundError, clubSupplyListNullError } from "../error";
+import { ClubHeadRepositoryImpl, ClubRepositoryImpl, UserRepositoryImpl } from "../repositories";
+import { clubNameExistError, clubNotFoundError, clubSupplyListNullError, userNotFoundError } from "../error";
 
 export default class ClubService {
     public clubList = async () => {
-        return ClubRepository.getQueryRepository().clubList();
+        return ClubRepositoryImpl.getQueryRepository().clubList();
     }
 
     public setBudget = async (id: number, budget: number) => {
         await this.notFoundError(id);
 
-        await ClubRepository.getQueryRepository().setBudget(id, budget);
+        await ClubRepositoryImpl.getQueryRepository().setBudget(id, budget);
     }
 
     public clubSupplyList = async (id: number, state: number) => {
         await this.notFoundError(id);
 
-        const list = await ClubRepository.getQueryRepository().findAllClubSupplyList(id, state);
+        const list = await ClubRepositoryImpl.getQueryRepository().findAllClubSupplyList(id, state);
         if(list.length === 0) throw clubSupplyListNullError;
         
         return list;
     }
 
+    public newClub = async (name: string, gcn: number) => {
+        const user = await UserRepositoryImpl.getQueryRepository().findUserByGcn(gcn);
+
+        if(!user) {
+            throw userNotFoundError;
+        }
+        if(await ClubRepositoryImpl.getQueryRepository().findClubByName(name)) {
+            throw clubNameExistError;
+        }
+
+        const club = await ClubRepositoryImpl.getQueryRepository().newClub(name);
+        await ClubHeadRepositoryImpl.getQueryRepository().newClubHead(club, user);
+    }
+
     private async notFoundError(id: number) {
-        if(!await ClubRepository.getQueryRepository().findOne(id)) {
+        if(!await ClubRepositoryImpl.getQueryRepository().findOne(id)) {
             throw clubNotFoundError;
         }
     }
